5ff269409c9b5f03432a7af4fd1bf1baaf0f6ef5
virtual424 <72723637+virtual424@users.noreply.github.com>
2022-06-14 11:04:52 +0530

    Update Shortest path in unweighted graph.java

--- GRAPH/Shortest path in unweighted graph.java
+++ GRAPH/Shortest path in unweighted graph.java
@@ -1,1 +1,22 @@
-
+ static void shortestPath(ArrayList<ArrayList<Integer>> adj,int V, int s, int[] dist){
+        boolean visited[] = new boolean[V];
+        
+        Queue<Integer> q = new LinkedList<>();
+        visited[s] = true;
+        q.add(s);
+        
+        while(!q.isEmpty()){
+            int u = q.poll();
+            for(int v:adj.get(u)){
+                if(visited[v] == false){
+                    dist[v] = dist[u]+1;
+                    visited[v] = true;
+                    q.add(v);
+                }
+            }
+        }
+        
+        for(int i=0;i<V;i++){
+            System.out.print(dist[i] + " ");
+        }
+    }
