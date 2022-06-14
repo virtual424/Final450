6689d57e3bedd083906d9e26d6d5875fb2ae6f78
virtual424 <72723637+virtual424@users.noreply.github.com>
2022-06-14 10:54:22 +0530

    Update Minimum steps by Knight.java

--- GRAPH/Minimum steps by Knight.java
+++ GRAPH/Minimum steps by Knight.java
@@ -1,1 +1,67 @@
+class Solution
+{
+    
+    
+    static class cell {
+        int x, y;
+        int dis;
+ 
+        public cell(int x, int y, int dis)
+        {
+            this.x = x;
+            this.y = y;
+            this.dis = dis;
+        }
+    }
+    
+    
 
+    public int minStepToReachTarget(int knightPos[], int targetPos[], int N)
+    {
+      int dx[] = { -2, -1, 1, 2, -2, -1, 1, 2 };
+        int dy[] = { -1, -2, -2, -1, 1, 2, 2, 1 };
+ 
+        
+        Queue<cell> q = new LinkedList<>();
+ 
+    
+        q.add(new cell(knightPos[0], knightPos[1], 0));
+ 
+        cell t;
+        int x, y;
+        boolean visit[][] = new boolean[N + 1][N + 1];
+     
+        visit[knightPos[0]][knightPos[1]] = true;
+ 
+        
+        while (!q.isEmpty()) {
+            t = q.poll();
+            
+            if (t.x == targetPos[0] && t.y == targetPos[1])
+                return t.dis;
+ 
+        
+            for (int i = 0; i < 8; i++) {
+                x = t.x + dx[i];
+                y = t.y + dy[i];
+ 
+            
+                if (isInside(x, y, N) && !visit[x][y]) {
+                    visit[x][y] = true;
+                    q.add(new cell(x, y, t.dis + 1));
+                }
+            }
+        }
+        return Integer.MAX_VALUE;   
+    }
+
+    
+     static boolean isInside(int x, int y, int N)
+    {
+        if (x >= 1 && x <= N && y >= 1 && y <= N)
+            return true;
+        return false;
+    }
+}
+            
+        
